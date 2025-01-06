75c69b49d0663640581fb19252eabcfb66f3b3ab
Yasir Raza <attishali366@gmail.com>
2025-01-06 16:44:27 +0500

    completed basic rag

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Constitution of Pakistan Chatbot',
+    version= '0.0.0',
+    author= 'Yasir Raza',
+    author_email= 'yasirabdali6@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
